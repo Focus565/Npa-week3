8ddcff6f72d0119e5e829dae7b23e75e641a8be7
focus <fogus251@hotmail.com>
2021-02-09 19:08:29 +0700

    Add class router

---
+++ router.py
@@ -0,0 +1,8 @@
+"""Router"""
+class Router:
+    
+    """create router"""
+    def __init__(self, brand, model, hostname):
+        self.brand = brand
+        self.model = model
+        self.hostname = hostname
