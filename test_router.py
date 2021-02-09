9854517424568d59b5068e91aa8538acecacd1f8
focus <fogus251@hotmail.com>
2021-02-09 19:06:22 +0700

    create test for init class

--- test_router.py
+++ test_router.py
@@ -1,3 +1,17 @@
 import pytest
 import router
-def test_create_class():
+def test_init():
+    r1 = router.Router('Cisco', 'IOSv', 'R1')
+    assert r1.brand == 'Cisco', "test failed"
+    assert r1.model == 'IOSv', "test failed"
+    assert r1.hostname == 'R1', "test failed"
+
+    r2 = router.Router('Cisco', '3745', 'R2')
+    assert r2.brand == 'Cisco', "test failed"
+    assert r2.model == '3745', "test failed"
+    assert r2.hostname == 'R2', "test failed"
+
+    r3 = router.Router('Juniper', 'Mx5', 'R3')
+    assert r3.brand == 'Juniper', "test failed"
+    assert r3.model == 'Mx5', "test failed"
+    assert r3.hostname == 'R3', "test failed"
