2acba239497005c0ff1d0f654ca932f0e7965c4b
focus <fogus251@hotmail.com>
2021-02-09 20:07:36 +0700

    add remove interface fuction

--- router.py
+++ router.py
@@ -11,3 +11,7 @@ class Router:
     def add_inf(self,interface_name):
         if interface_name not in self.interfaces:
             self.interfaces.append(interface_name)
+
+    def remove_inf(self,interface_name):
+        if interface_name in self.interfaces:
+            self.interfaces.remove(interface_name)
